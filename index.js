e9e99f28516a5382ea189030aa64a0eba91b9485
Chris Johns <chrisfotosmusic@gmail.com>
2021-04-18 21:55:34 -0400

    initial commit

---
+++ index.js
@@ -0,0 +1,13 @@
+
+
+console.log('running')
+
+let navs = Array.from(document.getElementsByTagName('nav'))
+navs.forEach(n => {
+    let children = Array.from(n.childNodes)
+    children.forEach(c => {
+        if(c.nodeName === 'A'){
+            c.tabIndex = '0'
+        }
+    })
+})
